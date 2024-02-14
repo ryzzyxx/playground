7e10e6b298b44c9e508489ba4edfbbd3b4f1cd15
ryzzyxx@gmail.com <ryzzyxx>
2024-02-14 11:04:55 -0600

    removed useless code.

--- Maui.Library.AppProject/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Maui.Library.AppProject;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- Maui.Library.AppProject/Maui.Library.Library/Platforms/iOS/PlatformClass1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Maui.Library.Library;
-
-// All the code in this file is only included on iOS.
-public class PlatformClass1
-{
-}
